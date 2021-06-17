5dbae55cefb73c440e08b4973495e1f1391673c6
Fred Liebenberg <fliebenberg@gmail.com>
2021-06-17 13:20:06 +0200

    added rest of existing bridge functions

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import { FS, FN } from './firebase';
 import { collectionData, docData } from 'rxfire/firestore';
-import { Observable } from 'rxjs';
-import { map, take } from 'rxjs/operators';
+import { combineLatest, merge, Observable, of } from 'rxjs';
+import { map, startWith, take } from 'rxjs/operators';
 import { Pair } from './models/pair.class';
-import { Order, OrderSide, OrderStatus, OrderType } from './models/order.class';
+import { Order, OrderSide, OrderStatus, OrderType, AggregateOrderEntry } from './models/order.class';
+import { Trade } from "./models/trade.class";
+import * as Utils from "./utils";
+import { TimeSlice } from './models/time-slice.class';
 
 const fb = FS();
 const fn = FN();
@@ -11,6 +14,7 @@ const fn = FN();
 const pairsCollectionRef = fb.collection('pairs');
 
 export { Pair, Order, OrderSide, OrderStatus, OrderType };
+export { Utils };
 
 // API
 // Returns an array all the tokens that can be exchanged.
@@ -126,4 +130,380 @@ export function getPairsMap$(): Observable<Map<string, Pair>> {
       return pairsMap;
     }),
   );
+}
+
+// API
+// Returns a list of aggregted Buys and Sells in the orderbook. 
+// Sell and Buy orders are sorted Ascending by price. Will only show "limit" results.
+export function getPairOrderBook$(pairId: string, limit: number = 40): Observable<{sells: AggregateOrderEntry[], buys: AggregateOrderEntry[]}> {
+  let buyOrderBook$ = getPairBuyOrders(pairId).pipe(
+      // aggregate orders per price
+      map((fullOrdersMap: Map<number, Order[]>) => {
+          return aggregateOrders(fullOrdersMap).slice(0, limit);
+      }),
+      startWith([]),
+  )
+  let sellOrderBook$ = getPairSellOrders(pairId).pipe(
+      // aggregate orders per price
+      map((fullOrdersMap: Map<number, Order[]>) => {
+          return aggregateOrders(fullOrdersMap).slice(0, limit).reverse();
+      }),
+      startWith([]),
+  )
+
+  return combineLatest([sellOrderBook$, buyOrderBook$]).pipe(
+      map(([sellOB, buyOB]) => {
+          return {sells: sellOB, buys: buyOB};
+      })
+  )
+}
+
+// API
+// Returns a price indexed map of all Buy orders for the specified pair.
+// Prices are sorted descending and orders are sorted by ascending dateCreated for each price.
+export function getPairBuyOrders(pairId: string): Observable<Map<number, Order[]>> {
+  return getPairOrdersMap("buy", pairId, Utils.SortOrder.DESCENDING);
+}
+
+// API
+// Returns a price indexed map of all Sell orders for the specified pair.
+// Prices are sorted ascending and orders are sorted by ascending dateCreated for each price.
+export function getPairSellOrders(pairId: string): Observable<Map<number, Order[]>> {
+  return getPairOrdersMap("sell", pairId, Utils.SortOrder.ASCENDING);
+}
+
+// API
+// Returns an array of trades for the specified pair.
+// Trades are sorted ASCENDING based on the trade date. Results are limited to the last "limit" trades - default and max is 1000. 
+// TODO Add in startAt parameter to allow extracting subsets of results.
+export function getPairTrades(pairId: string, limit: number = 1000): Observable<Trade[]> {
+  return getPairTradesByTime(pairId, 0, 0, limit);
+}
+
+// API
+// Returns an array of trades for the specified pair between the specified date/time.
+// Date/time is specified in milliseconds since UTC.
+// Trades are sorted ASCENDING based on the trade date. Results are limited to the last "limit" trades - default is 1000. 
+// TODO Add in startAt parameter to allow extracting subsets of results.
+export function getPairTradesByTime(pairId: string, startTime: number = 0, endTime: number = 0, limit: number = 1000): Observable<Trade[]> {
+  const maxLimit = 1000;
+  limit = Utils.roundTo(0, limit); // ensure limit is interger
+  limit = Math.min(limit, maxLimit);  // ensure limit is not larger than maxLimit
+  startTime = Utils.roundTo(0, startTime);  // ensure startTime is interger
+  endTime = Utils.roundTo(0, endTime);  // ensure endTime is integer
+  const tradesCollectionRef = fb.collection("pairs/"+pairId+"/trades");
+  const query = tradesCollectionRef
+  // .where("pair", "==", pairId)
+  .where("date", ">=", startTime)
+  .where("date", endTime == 0? ">=" : "<=", endTime)
+  .orderBy("date")
+  .limitToLast(limit);
+  return collectionData(query).pipe(
+      map((docs: any[]) => {
+          return docs.map((doc: any) => {
+              return Trade.create({...doc})
+          })
+      })
+  )
+}
+
+// API
+// Returns an array of slices for the specified pair.
+export function getPairSlices(pairId: string, limit: number = 1000): Observable<TimeSlice[]> {
+  return getPairSlicesByTime(pairId, 0, 0, limit);
+}
+
+// API
+// Returns an array of slices for the specified pair between the specified date/time.
+// Date/time is specified in milliseconds since UTC.
+// Slices are sorted ASCENDING based on the slice startTime. Results are limited to the last "limit" slices - default is 1000. 
+// TODO Add in startAt parameter to allow extracting subsets of results.
+export function getPairSlicesByTime(pairId: string, startTime: number = 0, endTime: number = 0, limit: number = 1000): Observable<TimeSlice[]> {
+  const maxLimit = 1000;
+  limit = Utils.roundTo(0, limit); // ensure limit is interger
+  limit = Math.min(limit, maxLimit);  // ensure limit is not larger than maxLimit
+  startTime = Utils.roundTo(0, startTime);  // ensure startTime is interger
+  endTime = Utils.roundTo(0, endTime);  // ensure endTime is integer
+  const query = fb.collection("pairs/"+pairId+"/slices")
+  .where("startTime", ">=", Utils.getTimeSliceStart(startTime))
+  .where("startTime", endTime == 0? ">=" : "<=", Utils.getTimeSliceStart(endTime))
+  .orderBy("startTime")
+  .limitToLast(limit);
+  return collectionData(query).pipe(
+      map((docs: any[]) => {
+          return docs.map((doc: any) => {
+              return new TimeSlice(doc.startTime, doc.open, doc.close, doc.high, doc.low, doc.token1Volume, doc.token2Volume, doc.noOfTrades);
+          })
+      })
+  )
+}
+  
+// API
+// Returns an array of trades for the specified wallet and the specified time period
+// Trades are sorted ASCENDING based on the trade date. Results are limited to the last "limit" trades - defualt is 1000.
+export function getWalletTrades(walletId: string, pairIds: string[], startTime: number = 0, endTime: number = 0, limit: number = 1000): Observable<Trade[]> {
+  // console.log("getWalletTrades starting...", walletId, pairIds);
+  const maxLimit = 1000;
+  limit = Utils.roundTo(0, limit); // ensure limit is interger
+  limit = Math.min(limit, maxLimit);  // ensure limit is not larger than maxLimit
+  startTime = Utils.roundTo(0, startTime);  // ensure startTime is interger
+  endTime = Utils.roundTo(0, endTime);  // ensure endTime is integer
+  let tradeObs: Observable<Trade[]>[] = [];
+  pairIds.forEach(pairId => {
+      // console.log("getWalletTrades: Creating obs for pair: "+pairId, pairIds)
+      const tradesCollectionRef = fb.collection("/pairs/"+ pairId +"/trades");
+      const query = tradesCollectionRef
+          .where("parties", "array-contains", walletId)
+          .where("date", ">=", startTime)
+          .where("date", endTime == 0? ">=" : "<=", endTime)
+          .orderBy("date")
+          .limitToLast(limit);
+      const pairTradeObs = collectionData(query).pipe(
+          map((docs: any[]) => {
+              // console.log("getWalletTrades: Found new trades for wallet "+ walletId, docs);
+              return docs.map((doc: any) => {
+                  const newTrade = Trade.create({...doc})
+                  // console.log("getWalletTrades: Found new trade for wallet "+ walletId, newTrade);
+                  return newTrade;
+              })
+          })
+      )
+      // console.log("getWalletTrades: Created pairTradesObs for pair "+ pairId, pairTradeObs);
+      tradeObs.push(pairTradeObs);
+  })
+  // console.log("getWalletTrades: Number of trade observables monitoring: ", tradeObs.length);
+  const result = merge(...tradeObs).pipe(
+      map(trades => {
+          // console.log("getWalletTrades updating trades: ", trades);
+          return trades;
+      })
+  );
+  return result;
+}
+
+// API
+// Returns a map with the fees earned in various tokens for the specified wallet and the specified time period.
+export function getWalletFees(walletId: string, pairIds: string[], startTime: number = 0, endTime: number = 0): Observable<{paid: Map<string, number>, earned: Map<string, number>}> {
+  // console.log("Getting wallet fees for wallet: "+ walletId);
+  startTime = Utils.roundTo(0, startTime);  // ensure startTime is interger
+  endTime = Utils.roundTo(0, endTime);  // ensure endTime is integer
+  const walletTrades = getWalletTrades(walletId, pairIds, startTime, endTime);
+  // const query = tradesCollectionRef
+  //     .where("parties", "array-contains", walletId)
+  //     .where("date", ">=", startTime)
+  //     .where("date", endTime == 0? ">=" : "<=", endTime)
+      // .orderBy("date")
+      // .limitToLast(limit);
+  return walletTrades.pipe(
+      map((trades: Trade[]) => {
+          // console.log("Starting to calc fees for trades: ", trades)
+          const feesPaidMap = new Map<string, number>();
+          const feesEarnedMap = new Map<string, number>();
+          trades.forEach((trade: Trade) => {
+              // const trade = Trade.create({...doc});
+              const feePayer = trade.feePayer == "BUYER"? trade.buyer : trade.seller;
+              if (feePayer == walletId) {
+                  // console.log("WalletId: "+ walletId +" matched the fee payer", trade);
+                  if (feesPaidMap.has(trade.feeToken)) {
+                      const oldFeesPaid = feesPaidMap.get(trade.feeToken) as number;
+                      feesPaidMap.set(trade.feeToken, oldFeesPaid + trade.platformFee + trade.liquidityFee);
+                    } else {
+                      feesPaidMap.set(trade.feeToken, trade.platformFee + trade.liquidityFee);
+                    }
+                  } else {
+                    if (feesEarnedMap.has(trade.feeToken)) {
+                      const oldFeesEarned = feesEarnedMap.get(trade.feeToken) as number;
+                      feesEarnedMap.set(trade.feeToken, oldFeesEarned + trade.liquidityFee);
+                  } else {
+                      feesEarnedMap.set(trade.feeToken, trade.liquidityFee);
+                  }
+              }
+          })
+          // console.log("wallet fees done. ", {paid: feesPaidMap, earned: feesEarnedMap});
+          return { paid: feesPaidMap, earned: feesEarnedMap} ;
+      })
+  )
+}
+
+// API
+// returns specified order based on order id
+export async function getOrder(orderId: string): Promise<Order | null> {
+  const orderPair = orderId.slice(0, orderId.search("_"));
+  const orderTypes = ["buy", "sell", "completed", "queued"];
+  let foundOrder = null;
+  let orderType = orderTypes.shift();
+  while (orderType && !foundOrder) {
+      const ordersArray = (await getPairOrdersArray(orderType, orderPair).pipe(take(1)).toPromise()); 
+      if (ordersArray) {
+        foundOrder = ordersArray.find(order => order.id == orderId);
+      }
+      if (!foundOrder) {orderType = orderTypes.shift()};
+  }
+  if (foundOrder) {
+      return foundOrder;
+  } else {
+      return null;
+  }
+}
+
+
+
+// UTILITY FUNCTIONS used in API bridge functions
+
+// Utility function - creates an aggregated orderbook array from an ordersMap;
+function aggregateOrders(ordersMap: Map<number, Order[]>): AggregateOrderEntry[] {
+  const aggOrdersArray: AggregateOrderEntry[] = [];
+  ordersMap.forEach((priceOrders: Order[], price: number) => {
+      if (priceOrders.length > 0) {
+          let totalQuantity = 0;
+          priceOrders.forEach((order: Order) => {
+              totalQuantity += order.quantity - order.quantityFulfilled;
+          })
+          const entry = {
+              pair: priceOrders[0].pair,
+              side: priceOrders[0].side,
+              price: price,
+              quantity: totalQuantity,
+              orderCount: priceOrders.length,
+          }
+          aggOrdersArray.push(entry);
+      }
+  })
+  return aggOrdersArray;
+
+}
+
+// Utility function - creates an ordersMap for the specified orderType for a pair
+function getPairOrdersMap(orderType: String, pairId: string, sortOrder: Utils.SortOrder): Observable<Map<number, Order[]>> {
+  const pairOrdersRef = fb.collection("pairs/"+pairId+"/"+orderType+"-orders");
+  return collectionData(pairOrdersRef).pipe(
+      map(orderDocs => {
+          let ordersMap = new Map<number, Order[]>();
+          orderDocs.forEach(orderDoc => {
+              const newOrder = Order.create(orderDoc);
+              if (!ordersMap.has(newOrder.price)) {
+                  ordersMap.set(newOrder.price, [newOrder]);
+              } else {
+                  ordersMap.get(newOrder.price)?.push(newOrder);
+              }
+          })
+          return Utils.sortOrdersMap(ordersMap, sortOrder);
+      })
+  )
+}
+// utility function - creates an array of the specified order type for the specified pair
+// the array can optionally be sorted by the specified field
+function getPairOrdersArray(
+  orderType: string, 
+  pairId: string, 
+  sortOrder: Utils.SortOrder = Utils.SortOrder.NONE, 
+  sortField: string = "dateCreated"
+): Observable<Order[]> {
+  const pairOrdersRef = fb.collection("pairs/"+pairId+"/"+orderType+"-orders");
+  return collectionData(pairOrdersRef).pipe(
+      map(orderDocs => {
+          return Utils.sortOrdersArray(orderDocs.map(orderDoc => {
+              return Order.create(orderDoc);
+          }), sortOrder, sortField);
+      })
+  )
+}
+
+// Utility function - calcs the quantity and value of tokens required for a MARKET order
+function calcMarketOrderRequirements(
+  order: Order, 
+  existingOrders: Map<number, Order[]>, 
+  priceLimit: number, 
+  token1Decimals: number, 
+  token2Decimals: number
+): {quantity: number, value: number} | null {
+  // console.log("Calculating order requirements for order: ", order);
+  // console.log("Pricelimit: ", priceLimit);
+  const orders = existingOrders;
+  let totalValue = 0;
+  let totalQuantity = 0;
+  let remainingValue = order.value;
+  let remainingQuantity = order.quantity;
+  let currentPriceIndex = 0;
+  let currentPrice = 0;
+  const orderPrices = Array.from(orders.keys());
+  if (orderPrices.length > 0) {
+      currentPrice = orderPrices[currentPriceIndex];
+  }
+  while (
+      (
+      (order.quantitySpecified && remainingQuantity > 0) || 
+      (!order.quantitySpecified && remainingValue > 0)
+      ) && 
+      currentPrice && 
+      (
+      (order.side == OrderSide.BUY && currentPrice <= priceLimit) || 
+      (order.side == OrderSide.SELL && currentPrice >= priceLimit)
+      )
+  ) {
+      const quantityAtPrice = Utils.roundTo(token1Decimals, Utils.calcPriceQuantity(orders.get(currentPrice)));
+      const valueAtPrice = Utils.roundTo(token2Decimals, quantityAtPrice * currentPrice);
+      if (order.quantitySpecified) {
+      if (quantityAtPrice >= remainingQuantity) {
+          totalValue = totalValue + Utils.roundTo(token2Decimals, remainingQuantity * currentPrice);
+          totalQuantity = totalQuantity + remainingQuantity;
+          remainingQuantity = 0;
+      } else {
+          totalValue = totalValue + valueAtPrice;
+          totalQuantity = totalQuantity + quantityAtPrice;
+          remainingQuantity = Utils.roundTo(token1Decimals, remainingQuantity - quantityAtPrice);
+      }
+      } else {
+      if (valueAtPrice > remainingValue) {
+          totalValue = totalValue + remainingValue;
+          totalQuantity = totalQuantity + Utils.roundTo(token1Decimals, remainingValue / currentPrice);
+          remainingValue = 0;
+      } else {
+          totalValue = totalValue + valueAtPrice;
+          totalQuantity = totalQuantity + quantityAtPrice;
+          remainingValue = Utils.roundTo(token2Decimals, remainingValue - valueAtPrice);
+      }
+      }
+      currentPriceIndex ++;
+      if (currentPriceIndex < orderPrices.length) {
+      currentPrice = orderPrices[currentPriceIndex];
+      } else {
+      currentPrice = 0;
+      }
+  }
+  if ((order.quantitySpecified && remainingQuantity > 0)
+      || (!order.quantitySpecified && remainingValue > 0)
+  ) {
+      // return {quantity: 0, value: 0};
+      return null;
+  } else {
+      return {quantity: totalQuantity, value: totalValue};
+  }
+}
+
+// these functions are only for testing and will not be included in the production version
+
+export function getWallets(wallets: string[]): Map<string,Observable<Map<string, number>>> {
+  const walletMap = new Map<string, Observable<Map<string, number>>>();
+  const walletsRef = fb.collection("wallets");
+  wallets.forEach(wallet => {
+      walletMap.set(wallet, getWalletTokens(wallet).pipe(startWith(new Map())));
+  })
+  return walletMap;
+}
+
+export function getWalletTokens(wallet: string): Observable<Map<string, number>> {
+  if (!wallet) {
+      return of(new Map());
+  }
+  const walletTokensRef = fb.collection("wallets/" + wallet + "/tokens");
+  return collectionData(walletTokensRef, "tokenId").pipe(
+      map((docs: any[]) => {
+          let tokensMap = new Map<string, number>();
+          docs.forEach((doc: any) => {
+              tokensMap.set(doc["tokenId"], doc.owned);
+          })
+          return tokensMap;
+      })
+  )
 }
---
+++ src/utils.ts
@@ -0,0 +1,86 @@
+import { from } from "rxjs";
+import { Order, OrderSide } from "./models/order.class";
+
+export enum SortOrder {
+    ASCENDING = "ASCENDING",
+    DESCENDING = "DESCENDING",
+    NONE = "NONE"
+}
+
+function createOrdersMap(order: Order, orderObjs: any[]): Map<number, Order[]> {
+  const ordersMap = new Map<number, Order[]>();
+  orderObjs.forEach((orderObj: any) => {
+    const tOrder = Order.create(orderObj);
+    if (!ordersMap.has(tOrder.price)) {
+      ordersMap.set(tOrder.price, [tOrder]);
+    } else {
+      ordersMap.get(tOrder.price)?.push(tOrder);
+    }
+  });
+  const sort = order.side == OrderSide.BUY ? SortOrder.ASCENDING : SortOrder.DESCENDING;
+  return sortOrdersMap(ordersMap, sort);
+}
+
+export function sortOrdersMap(ordersMap: Map<number, Order[]>, sortOrder: SortOrder = SortOrder.ASCENDING): Map<number, Order[]> {
+  const newOrdersMap = new Map<number, Order[]>();
+  let orderedKeys = Array.from(ordersMap.keys()).sort((a, b) => a-b);
+  if (sortOrder == SortOrder.DESCENDING) {
+    orderedKeys = orderedKeys.reverse();
+  }
+  orderedKeys.forEach((key) => {
+    const newOrdersList = sortOrdersArray(ordersMap.get(key));
+    newOrdersMap.set(key, newOrdersList);
+  });
+  return newOrdersMap;
+}
+
+export function sortOrdersArray(ordersArray: Order[] | undefined, sortOrder: SortOrder = SortOrder.ASCENDING, field: string = "dateCreated"): Order[] {
+  if (!ordersArray) {
+    return []
+  } else if (sortOrder == SortOrder.NONE) {
+    return ordersArray;
+  } else {
+    const sortMultiplier = sortOrder == SortOrder.ASCENDING ? 1 : -1;
+    const newOrdersArray = ordersArray.sort((a: any, b: any) => {
+      return (a[field] - b[field])*sortMultiplier;
+    });
+    return newOrdersArray;
+  }
+}
+
+export function calcPriceQuantity(ordersArray: Order[] | undefined): number {
+  if (ordersArray) {
+    return ordersArray.reduce((total, order) => {
+      return total + order.quantity - order.quantityFulfilled;
+    }, 0);
+  } else return 0;
+}
+
+export function roundTo(digits: number=0, n: number): number {
+    let negativeMultiplier = n < 0 ? -1 : 1;
+    const multiplier = Math.pow(10, digits) * negativeMultiplier;
+    const result = +(Math.round(n * multiplier) / multiplier).toFixed(digits);
+    return result;
+}
+
+export function getLastElement <T>(a: T[] | undefined): T | null {
+  if (a && a.length > 0) {
+    return a[a.length-1];
+  } else {
+    return null;
+  }
+}
+
+export function getTimeSliceStart(date: number, TSDuration: number = 15): number {
+  const timeSliceSize = TSDuration * 60 * 1000;
+  return date - (date % timeSliceSize);
+}
+
+
+export function getTokenNameFromPair(pairCode: string, tokenNo: "token1" | "token2"): string {
+  if (tokenNo == "token1") {
+    return pairCode.split("-")[0];
+  } else {
+    return pairCode.split("-")[1];
+  }
+}
